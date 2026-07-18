66e26150e2cf505f3335f4445fd77f86a8c9d4b8
sudhiksha-prabhakar <154581962+sudhiksha-prabhakar@users.noreply.github.com>
2026-07-18 16:16:05 +0530

    Time: 0 ms (100%), Space: 12.3 MB (88.29%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        count=0
+        for stone in stones:
+            if stone in jewels:
+                count+=1
+        return count        
+
+        
