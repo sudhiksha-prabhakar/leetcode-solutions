288d54d6ef106d963a11427212347f12123d7cb1
sudhiksha-prabhakar <154581962+sudhiksha-prabhakar@users.noreply.github.com>
2026-08-05 20:09:32 +0530

    Time: 0 ms (100%), Space: 12.4 MB (19.08%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -1,16 +1,16 @@
 class Solution(object):
     def buildArray(self, target, n):
-        ans = []
-        i = 0
+        ans=[]
+        i=0
 
-        for num in range(1, n + 1):
+        for num in range(1,n+1):
             ans.append("Push")
 
-            if num == target[i]:
-                i += 1
-                if i == len(target):
+            if num==target[i]:
+                i+=1
+                if i==len(target):
                     break
+
             else:
                 ans.append("Pop")
-
-        return ans
+        return ans                
