a693b85167eb8e7ef3eab6093473d0eee77575d1
Sudhiksha Prabhakar <154581962+sudhiksha-prabhakar@users.noreply.github.com>
2026-09-07 12:15:56 +0530

    Time: 0 ms (100%), Space: 12.4 MB (57.34%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,16 +1,18 @@
+from collections import deque
+
 class Solution(object):
     def countStudents(self, students, sandwiches):
-        q=deque(students)
-        i=0
+        q = deque(students)
+        i = 0
 
         while q:
-            if q[0]==sandwiches[i]:
+            if q[0] == sandwiches[i]:
                 q.popleft()
-                i+=1
+                i += 1
             else:
                 q.append(q.popleft())
 
-            if all(student!=sandwiches[i] for student in q):
-                break
-        return len(q)                
-        
+                if all(student != sandwiches[i] for student in q):
+                    break
+
+        return len(q)
