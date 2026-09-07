03cdfe250ca3b2dd3217b94eb71981983208324d
Sudhiksha Prabhakar <154581962+sudhiksha-prabhakar@users.noreply.github.com>
2026-09-07 12:19:46 +0530

    Time: 0 ms (100%), Space: 12.4 MB (54.75%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def clearDigits(self, s):
+        stack=[]
+
+        for ch in s:
+            if ch.isdigit():
+                stack.pop()
+            else:
+                stack.append(ch)
+
+        return "".join(stack)            
+        
